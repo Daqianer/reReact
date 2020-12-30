dfb54a62984bfe9a0232768402a12b16eb010148
daqianer <daqianer@daqianerdeMacBook-Pro.local>
2020-12-30 14:06:22 +0800

    P54

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,14 +2,15 @@ import React, { Component } from "react";
 import Counter from "./counter";
 class Counters extends Component {
   render() {
+    const { onReset, onDelete, onIncrement, counters } = this.props;
     return (
       <div>
-        <button onClick={this.props.onReset}>Reset</button>
-        {this.props.counters.map((item) => (
+        <button onClick={onReset}>Reset</button>
+        {counters.map((item) => (
           <Counter
             key={item.id}
-            onIncrement={this.props.onIncrement}
-            onDelete={this.props.onDelete}
+            onIncrement={onIncrement}
+            onDelete={onDelete}
             counter={item}
           />
         ))}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,14 @@
-import React, { Component } from "react";
-
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <span className="navbar-brand mb-0 h1">
-          Navbar
-          <span className="badge badge-pill badge-secondary">
-            {this.props.countersNum}
-          </span>
-        </span>
-      </nav>
-    );
-  }
-}
+import React from "react";
+// 无状态功能性组件
+const Navbar = ({ countersNum }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <span className="navbar-brand mb-0 h1">
+        Navbar
+        <span className="badge badge-pill badge-secondary">{countersNum}</span>
+      </span>
+    </nav>
+  );
+};
 
 export default Navbar;
